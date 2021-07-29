1890801b0e1679149ad79294aa7f252f435179c2
alexyamm33 <ayeh0330@gmail.com>
2021-07-29 10:51:48 -0400

    Initial Commit

---
+++ UMasked?Tests/UMasked_Tests.swift
@@ -0,0 +1,33 @@
+//
+//  UMasked_Tests.swift
+//  UMasked?Tests
+//
+//  Created by Alex Yeh on 2021-07-29.
+//
+
+import XCTest
+@testable import UMasked_
+
+class UMasked_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
